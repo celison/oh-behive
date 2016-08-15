3905d52d6406bd898af46de6ad803c616d51c956
Connor Elison <cjelison@gmail.com>
2016-08-14 18:13:05 -0700

    rename

---
+++ src/main/java/drocck/sp/beesandhoney/business/entities/DTOs/PolliShipmentCreateDTO.java
@@ -0,0 +1,23 @@
+package drocck.sp.beesandhoney.business.entities.DTOs;
+
+import drocck.sp.beesandhoney.business.entities.PolliShipment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Robert on 3/20/2016.
+
+ needs fixed
+ */
+public class PolliShipmentCreateDTO {
+    private List<String> directions = new ArrayList<>();
+
+    public List<String> getDirections() {
+        return directions;
+    }
+
+    public void setDirections(List<String> directions) {
+        this.directions = directions;
+    }
+}
